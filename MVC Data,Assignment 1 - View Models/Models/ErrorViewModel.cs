5ffb89014b14f865d9eb73a6ef68004912d551a6
Stefan Niskanen Skoglund <stefan.skoglund@agj.net>
2021-09-04 00:40:55 +0200

    projektskapning
    ett C# proj för själva servern/projektet

---
+++ MVC Data,Assignment 1 - View Models/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MVC_Data_Assignment_1___View_Models.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
